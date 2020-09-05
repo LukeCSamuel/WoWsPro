bd7fd6d860fad63ca4f65008276a77c97f4eaa06
Luke Caswell Samuel <luke.caswell.samuel@gmail.com>
2020-09-05 16:08:36 -0400

    added TeamRoster component

--- WoWsPro.Shared/Constants/TeamStatus.cs
+++ WoWsPro.Shared/Constants/TeamStatus.cs
@@ -13,4 +13,28 @@ namespace WoWsPro.Shared.Constants
 		Withdrawn,
 		Disqualified
 	}
+
+	public static class TeamStatusExtensions
+	{
+		public static string ToVerb (this TeamStatus status)
+		{
+			switch (status)
+			{
+			case TeamStatus.Unregistered:
+				return "Unregistered";
+			case TeamStatus.Registered:
+				return "Registered";
+			case TeamStatus.WaitListed:
+				return "Wait Listed";
+			case TeamStatus.Accepted:
+				return "Accepted";
+			case TeamStatus.Withdrawn:
+				return "Withdrawn";
+			case TeamStatus.Disqualified:
+				return "Disqualified";
+			default:
+				throw new NotImplementedException("TeamStatus of " + status + " does not have a ToVerb extension implementation.");
+			}
+		}
+	}
 }
